5f79ca98efe8bc3115faa75c4e7d9ba033f0afd9
rcgiovana <giovana.45cardoso@gmail.com>
2023-05-27 19:22:29 -0300

    ensure loginRouter retruns if password is provived

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.passoword) {
       return {
         statusCode: 400
       }
@@ -15,6 +15,18 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         passoword: 'any_pass'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should return 400 if no password is provived', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
